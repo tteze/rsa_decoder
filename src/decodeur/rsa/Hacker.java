877d9222654825517dd3e29a69ed4c1b95ab7da8
Théophile <Thophile@tt>
2015-02-09 09:51:31 -0500

    em;m

--- src/decodeur/rsa/Hacker.java
+++ src/decodeur/rsa/Hacker.java
@@ -17,7 +17,9 @@ public class Hacker {
     }
     public boolean intercept(BigInteger[] message,BigInteger n_dest,BigInteger e_dest,int size){
         try{ // on essaye de cracker
-            crack(message,n_dest,e_dest,size);
+            String freq;
+            freq=crack(message,size);
+            System.out.println("Message cracké par fréquence: "+freq);
         }
         catch(Exception e){ //on retourne l'échec du crack
             return false;
@@ -25,8 +27,54 @@ public class Hacker {
         return true; // crack résussi
     }  
 
-    private void crack(BigInteger[] message,BigInteger n_dest,BigInteger e_dest,int size){
-        // algorithme de crack
+    private String crack(BigInteger[] message,int size){
+        // algorithme de crack par fréquences
+        //on trie les lettres par oredres d'occurences
+        int taille=0;
+        boolean ajout;
+        BigInteger[]trie= new BigInteger[size];
+        int[] occurence=new int[size];
+        
+        for(int i=0;i<size;i++){
+            occurence[i]=0;
+        }
+        for(int i=0;i<size;i++){        //on compte les occurences
+            ajout=true;
+            for(int a=0;a<taille;a++){
+                if(message[i].equals(trie[a])){
+                    occurence[a]++;
+                    ajout=false;
+                }
+            }
+            if(ajout){
+                trie[taille]=message[i];
+                taille++;
+            }
+        }
+        for(int i=0;i<taille;i++){      // on trie par ordre d'occurences
+            int temp=occurence[i];
+            BigInteger temp2=trie[i];
+            int j=i;
+            while(j>0 && occurence[j-1]<temp){
+                occurence[j]=occurence[j-1];
+                trie[j]=trie[j-1];
+                j=j-1;
+            }
+            occurence[j]=temp;
+            trie[j]=temp2;
+        }
+        String s;                           // on compare avec l'alphabet
+        char[] c=new char[size];
+        char[] francais={' ','e','s','a','i','n','t','r','u','l','o','d','c','p','m','v','q','g','f','h','b','x','j','y','z','k','w'};
+        for(int i=0;i<size;i++){
+            for(int j=0;j<taille;j++){
+                if(message[i].equals(trie[j])){
+                    c[i]=francais[j];
+                }
+            }
+        }
+        s=String.copyValueOf(c);
+        return s;
     }
     
 }
